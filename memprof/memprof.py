500b05fa5bf9db0be936c40858cdddf500601a4e
Jose M. Dana <dana.develop@gmail.com>
2013-08-17 19:00:30 +0100

    GPLv3 statement added to every single python file

--- memprof/memprof.py
+++ memprof/memprof.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2013 Jose M. Dana
+#
+# This file is part of memprof.
+#
+# memprof is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation (version 3 of the License only).
+#
+# memprof is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with memprof.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import time
 import copy
